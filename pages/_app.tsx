3a5b1e4ba653f6c1ffbff309afcc9d3a2319d1e9
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-27 02:03:35 +0600

    app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import type { AppProps } from "next/app";
 import "../global.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>;
 }
 
 export default MyApp;
