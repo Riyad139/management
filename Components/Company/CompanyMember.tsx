8bd8ffc945d592f30e3ca39b5d538f109224ee20
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-27 23:34:15 +0600

    fix: CompanyMember prop types

--- Components/Company/CompanyMember.tsx
+++ Components/Company/CompanyMember.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-export default function CompanyMember(props: String) {
+export default function CompanyMember(props: {img:string}) {
   return (
     <div className="w-9 h-9 flex items-center justify-center p-1 -ml-3 border bg-white rounded-full">
       <img
