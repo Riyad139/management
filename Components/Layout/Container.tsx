92111b06bba88745e52dc7d0fce6e5f6bf2a7728
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-27 23:37:04 +0600

    fix layout props type

--- Components/Layout/Container.tsx
+++ Components/Layout/Container.tsx
@@ -1,3 +1,5 @@
-export default function Container(props) {
+import { ReactNode } from "react";
+
+export default function Container(props: { children: ReactNode }) {
   return <div className="mx-4 my-10">{props.children}</div>;
 }
