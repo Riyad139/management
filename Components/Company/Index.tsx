5bee057ba72d080412083fdd189791be196c0cc9
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-28 19:43:34 +0600

    update

--- Components/Company/Index.tsx
+++ Components/Company/Index.tsx
@@ -9,13 +9,17 @@ import { useQuery } from 'react-query'
 import { ICompany } from '../../@types/ICompany'
 import api from '../../library/axiosClient'
 import { Iuser } from '../../@types/Iuser'
+import { Iproject } from '../../@types/Iproject'
 
 export default function Company(company: ICompany) {
    const [isFavourite, setFavourite] = useState(false)
    const memberIds = company.members.map(mem => mem.userId)
-
+   const projectIds = company.projects
    const userRes = useQuery('users', () =>
       api.get<Iuser[]>('/users', { data: { userIds: memberIds } })
+   )
+   const projectRes = useQuery('projects', () =>
+      api.get<Iproject[]>('/projects', { data: { projectId: projectIds } })
    )
 
    const favouriteHandler = () => {
@@ -57,17 +61,20 @@ export default function Company(company: ICompany) {
             {/* // top part ends here  */}
             {/* priority section start */}
             <div className=" text-sm justify-between flex mt-8 mb-5 pr-4">
-               <div className="px-2 py-1 text-gray-400 border bg-slate-50 rounded-lg">
+               <div className="px-2 py-1 text-gray-400 border bg-slate-50 rounded-md">
                   <p className="uppercase font-semibold">in progress</p>
                </div>
-               <div className="px-2 py-1 text-orange-500 border border-orange-500 rounded-lg">
-                  <p>High priority</p>
+               <div className="px-2 py-1 text-orange-500 border border-orange-500 rounded-md">
+                  <p className="uppercase">{projectRes.data?.data[0].priority}</p>
                </div>
             </div>
             {/* priority ends here */}
             {/* task starts here */}
             <div className="pr-5 space-y-5">
-               <p className="text-sm text-gray-600">task done : 25/50</p>
+               <p className="text-sm text-gray-600">
+                  task done : {projectRes.data?.data[0].taskDone}/
+                  {projectRes.data?.data[0].totalTask}
+               </p>
                <div className="">
                   <LinearProgress variant="determinate" value={50} />
                </div>
@@ -76,10 +83,10 @@ export default function Company(company: ICompany) {
             {/* tags */}
             <div className="mt-4 flex text-xs space-x-3">
                <div className="px-3 border border-green-300 text-green-600 bg-green-50 rounded-md py-1 ">
-                  <p className="uppercase ">IOS App</p>
+                  <p className="uppercase ">{projectRes.data?.data[0].tags[0]}</p>
                </div>
                <div className="px-3 border border-blue-300 text-blue-600 bg-blue-50 py-1 rounded-md ">
-                  <p className="uppercase ">UI/UX</p>
+                  <p className="uppercase ">{projectRes.data?.data[0].tags[1]}</p>
                </div>
             </div>
             {/* tags */}
@@ -95,7 +102,9 @@ export default function Company(company: ICompany) {
 
             {/* members */}
             <div className="mt-7 px-4 py-2 border rounded-md border-orange-300 bg-orange-50 text-orange-600 inline-block">
-               <p className="uppercase text-xs ">due date : 20 November</p>
+               <p className="uppercase text-xs ">
+                  due date : {projectRes.data?.data[0].dueDate}
+               </p>
             </div>
          </div>
       </Container>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { useQuery } from 'react-query'
 
 const Home: NextPage = () => {
    const companies = useQuery('getcompanies', () => api.get('/companies'))
-   console.log(companies)
+   
 
    return (
       <div className="p-2">
