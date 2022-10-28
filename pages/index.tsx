9e7dd814e5a71db8bbdde04611f443098425328f
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-29 04:07:13 +0600

    small updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,21 @@ import { useQuery } from 'react-query'
 import Mytask from '../Components/Mytask'
 import { Itask } from '../@types/Itask'
 import TaskByCompany from '../Components/Mytask/TaskByCompany'
+import Calender from '../Components/Calendar'
+import { Imeetings } from '../@types/Imeetings'
 
 const Home: NextPage = () => {
    const companies = useQuery('getcompanies', () => api.get('/companies'))
    const taskRes = useQuery('tasks', () => api.get<Itask[]>('/tasks'))
-   
-   if (!taskRes.data?.data) return null
+   const meetings = useQuery('meetings', () => api.get<Imeetings[]>('/meetings'))
+   if (!taskRes.data?.data || !meetings.data?.data) return null
 
    return (
       <div className="p-2">
          <SearchBar />
 
          {/* @TODO typing response */}
-         <div className="flex space-x-5">
+         <div className="flex space-x-7">
             <div>
                {companies.data?.data.map((company: any) => (
                   <Company {...company} />
@@ -27,6 +29,9 @@ const Home: NextPage = () => {
             <div>
                <Mytask taskType="My Task" tasks={taskRes.data.data} />
                <TaskByCompany company={companies.data?.data} tasks={taskRes.data?.data} />
+            </div>
+            <div>
+               <Calender meetings={meetings.data?.data} />
             </div>
          </div>
       </div>
