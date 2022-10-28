a78d763d959bffafa5710f53f4223fb64e15df4e
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-29 01:59:06 +0600

    add Itask

---
+++ @types/Itask.d.ts
@@ -0,0 +1,14 @@
+export interface Itask {
+    id: string
+    name: string
+    timeStamps: string
+    board: string
+    description: string
+    lablels: string
+    isDone: boolean
+    attachments: { originalName: string; fileId: string }[]
+    assignTo: string[]
+    createdBy: string
+    coverImage: string
+    deadLine: string
+}
