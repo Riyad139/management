4ad1384bf33d30f9a07998981c5daea8e17a85b9
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-28 18:52:42 +0600

    adding iproject interface

---
+++ @types/Iproject.d.ts
@@ -0,0 +1,14 @@
+export interface Iproject {
+    projectId: string
+    tasks: string[]
+    priority: string
+    totalTask: number
+    taskDone: number
+    projectStatus: string
+    tags: string[]
+    name: string
+    dueDate: string
+    createdBy: string
+    createdAt: string
+    updatedAt: string
+}
