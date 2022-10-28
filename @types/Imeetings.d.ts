d4485fb5233d999efc2df97833dbe0bcd41ac306
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-29 04:06:22 +0600

    adding imeetings

---
+++ @types/Imeetings.d.ts
@@ -0,0 +1,10 @@
+export interface Imeetings {
+    _id: string
+    hostedBy: string
+    timeStamps: string
+    title: string
+    descriptions: string
+    duration: number
+    invitedUser: string[]
+    meetingDate: date
+}
