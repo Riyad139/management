24b4ac92e3b985b1e09f119b9fad927f109c483e
Riyad139 <riyad.ahmed1886@gmail.com>
2022-10-30 01:21:07 +0600

    Calender update

--- Components/Calendar/index.tsx
+++ Components/Calendar/index.tsx
@@ -1,25 +1,21 @@
 import Card from '../Card'
-import { Calendar } from 'react-date-range'
-import 'react-date-range/dist/styles.css' // main style file
-import 'react-date-range/dist/theme/default.css' // theme css file
-import { DateRange } from 'react-date-range'
+import React from 'react'
+import { format } from 'date-fns'
+import { DayPicker } from 'react-day-picker'
+import 'react-day-picker/dist/style.css'
 import { Imeetings } from '../../@types/Imeetings'
+import { useEffect, useState } from 'react'
 
 export default function Calender(props: { meetings: Imeetings[] }) {
-   const range = props.meetings.map(mt => {
-      const obg = {
-         startDate: new Date(mt.meetingDate),
-         endDate: new Date(mt.meetingDate),
-         key: 'selection',
-      }
-      return obg
-   })
-   if (!range) return null
-   console.log(range)
+   const initDays: Date[] = props.meetings.map(mt => new Date(mt.meetingDate))
+
+   const [days, setdays] = useState(initDays)
+
+   // if (subRange != null) range.push(subRange)
 
    return (
       <div className="p-5 my-10 rounded-lg shadow-sm bg-white">
-         <DateRange date ranges={range} />
+         <DayPicker mode="multiple" min={1} selected={days} onSelect={setdays} />
       </div>
    )
 }
